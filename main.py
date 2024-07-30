5260b3f9d78395c9d8d70e788cbe0ab592882356
cryptogun <safetychinese@gmail.com>
2024-07-29 23:17:01 -0230

    feat: 1. impl db observer pattern. 2. change to secure mode: encrypt tasks & hash passwords. 3. prettify: add image background.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from views.main_view import ToDoApp
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("750x500")
-    root.title('Todo List')
+    root.title('Secure TodoList')
     app = ToDoApp(root)
     root.mainloop()
 
--- views/login_register_view.py
+++ views/login_register_view.py
@@ -2,15 +2,35 @@ import tkinter as _tk
 from tkinter import ttk as tk
 from tkinter import messagebox
 from controllers.user_controller import UserController
+from tkinter import TclError
+from tkinter import PhotoImage
+import os
 
 class LoginRegisterWindow:
     def __init__(self, root, to_do_app,user_controller):
         self.root = root
+        self.root.geometry("580x380")
         self.to_do_app = to_do_app
         self.user_controller = user_controller
 
-        self.login_frame = tk.Frame(root)
-        self.login_frame.pack(pady=10)
+        cover_path = os.sep.join([
+            os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+            "resources/purple_book.png"
+        ])
+
+        # Load the image
+        self.bg = PhotoImage(file=cover_path)
+
+        # Create a canvas
+        self.canvas = _tk.Canvas(root, width=580, height=380)
+        self.canvas.pack(fill="both", expand=True)
+        
+        # Add the image to the canvas
+        self.canvas.create_image(-30, -60, image=self.bg, anchor="nw")
+
+        # Create a frame
+        self.login_frame = _tk.Frame(root, background='#C5E9F1')
+        self.login_frame.place(relx=0.5, rely=0.5, anchor="center")
 
         tk.Label(self.login_frame, text="Username").grid(row=0, column=0)
         self.username_entry = tk.Entry(self.login_frame)
@@ -28,7 +48,7 @@ class LoginRegisterWindow:
 
         self.register_btn = tk.Button(self.login_frame, text="Register", command=self.register)
         self.register_btn.grid(row=3, column=0, columnspan=2)
-        self.login_btn.bind('<Return>', self.register)
+        self.register_btn.bind('<Return>', self.register)
 
 
     def login(self, event=None):
@@ -37,11 +57,12 @@ class LoginRegisterWindow:
         success, message = self.user_controller.login_user(username, password)
         if success:
             showMessage(message, type='success', timeout=2000)
+            self.canvas.destroy()  # Destroy the login canvas
             self.to_do_app.show_main_window()
         else:
             showMessage(message, type='error')
 
-    def register(self):
+    def register(self, event=None):
         username = self.username_entry.get()
         password = self.password_entry.get()
         success, message = self.user_controller.register_user(username, password)
